8f0c0c0bdd644eec7e0d69f86236a0878da0813d
Mikko Ahola <mikko.ahola@solidabis.com>
2022-03-13 20:13:45 +0200

    update main page text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploy react app to aws poc
         </p>
         <a
           className="App-link"
